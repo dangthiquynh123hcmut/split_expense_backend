de771cd16cc6e4429c5480e3e25b9d22d48bbe04
quynhdang <quynh.dangthi2212880@hcmut.edu.vn>
2025-08-18 20:26:46 +0700

    fix(render): static file

--- src/split_expense_system/urls.py
+++ src/split_expense_system/urls.py
@@ -1,15 +1,29 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path
+from django.views.decorators.http import require_GET
 
 from utils.router.api import BaseAPI
+
+
+@require_GET
+def root_view(request):
+    return JsonResponse(
+        {
+            "message": "Welcome to Split Expense API",
+            "documentation": "Please use /api/ to access the API endpoints",
+            "admin": "/admin/ for admin interface",
+        }
+    )
 
 
 api = BaseAPI()
 api.auto_discover_controllers()
 
 urlpatterns = [
+    path("", root_view, name="root"),
     path("admin/", admin.site.urls),
     path("api/", api.urls),
 ]
